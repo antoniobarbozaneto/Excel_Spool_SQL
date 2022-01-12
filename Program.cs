740ff354e721dda0a910f658aaf6e644209e84d3
Antonio Barboza Neto <neto.barboza1995@gmail.com>
2022-01-11 22:22:01 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_GerarScriptInsert_Excel
+{
+
+    public class ConsoleApp_GerarScriptInsert_Excel
+    {
+        static void Main(string[] args)
+        {
+           var path = System.IO.Path.GetDirectoryName("C:\\Users\\neto_\\source\\repos\\Excel\\"); //Diretório do arquivo de entrada e saída
+
+            var products = CreateProductsScript(path);
+            System.IO.File.WriteAllText(System.IO.Path.Combine(path, "sugestoes.sql"), products); //faz o Spool do arquivo .sql, passando o caminho de saída, o nome do arquivo + extensão, e o dados.
+            Console.WriteLine("Arquivo gerado com sucesso!!!");
+            Console.ReadKey();
+            //
+
+        }
+
+            public static string CreateProductsScript(string path)
+            {
+
+                var workbook = new XLWorkbook(path + "\\recomendation.xlsx"); //Concatena o caminho + o nome do seu arquivo Excel de entrada (não esquece da extensão do arquivo!)
+                var ws1 = workbook.Worksheet("recomendation"); //Passa o nome da Sheet do seu arquivo que você vai querer manipular
+
+                var rows = ws1.RowsUsed();
+
+                var sb = new StringBuilder();
+
+                int counter = 0;
+
+                Console.WriteLine("Lendo o Excel...");
+
+                foreach (var row in rows)
+                {
+                    if (row.RowNumber() > 1)
+                    {
+                        counter++;
+
+                        var id = Convert.ToString(row.Cell(1).Value); //le a 1 coluna
+                        var name = Convert.ToString(row.Cell(2).Value); //le a 2 coluna
+
+                        // executing
+
+                        sb.AppendLine("INSERT INTO suggestionbox (id,name) VALUES(" + id + ",'" + name + "');"); //concatena as variaveis com as informações pra montar o Insert.
+
+                    }
+                }
+
+                return sb.ToString();
+            }
+
+            /*
+            protected string Trim(string text)
+            {
+                if (string.IsNullOrEmpty(text))
+                    return string.Empty;
+
+                return text.Trim();
+            }
+            */   
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("ConsoleApp_GerarScriptInsert_Excel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp_GerarScriptInsert_Excel")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("077699ea-7287-4504-9f8c-bdb93892f4c0")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
